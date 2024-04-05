fbf07bd6c610a4db47e26a6828377e8897a470cd
SRaswan <shauryaraswan@gmail.com>
2024-04-05 00:14:52 -0700

    FIRST COMMIT

---
+++ trader1.py
@@ -0,0 +1,49 @@
+from datamodel import OrderDepth, UserId, TradingState, Order
+from typing import List
+import string
+
+class Trader:
+    def goodprice(self, order_depth):
+        goodprice = 10
+        if len(order_depth.sell_orders) != 0 and len(order_depth.buy_orders) != 0:
+            sellprice = sum(q * p for q, p in order_depth.sell_orders.items()) / sum(order_depth.sell_orders.values())
+            buyprice = sum(q * -p for q, p in order_depth.buy_orders.items()) / -sum(order_depth.buy_orders.values())
+            midprice = (sellprice+buyprice) / 2
+            goodprice = midprice
+        return goodprice
+        
+    def run(self, state: TradingState):
+        # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
+        print("traderData: " + state.traderData)
+        print("Observations: " + str(state.observations))
+        result = {}
+        for product in state.order_depths:
+            order_depth: OrderDepth = state.order_depths[product]
+            orders: List[Order] = []
+            # BRAINDEAD CALCULATION FOR ACCEPTABLE PRICE
+            acceptable_price = self.goodprice(order_depth);  # Participant should calculate this value
+            print("Acceptable price : " + str(acceptable_price))
+            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+    
+            if len(order_depth.sell_orders) != 0:
+                # Min ask price is the first one
+                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                if int(best_ask) < acceptable_price:
+                    print("BUY", str(-best_ask_amount) + "x", best_ask)
+                    orders.append(Order(product, best_ask, -best_ask_amount-1))
+    
+            if len(order_depth.buy_orders) != 0:
+                # Max bid price is the first one
+                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                if int(best_bid) > acceptable_price:
+                    print("SELL", str(best_bid_amount) + "x", best_bid)
+                    orders.append(Order(product, best_bid, -best_bid_amount))
+            
+            result[product] = orders
+    
+    
+        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+        
+        conversions = 1
+        return result, conversions, traderData
+    
